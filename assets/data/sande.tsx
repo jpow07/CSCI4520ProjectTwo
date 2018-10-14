ba8d40de4dd3167a13bbd7235b585f45e4698b42
Alexaa5 <43099240+Alexaa5@users.noreply.github.com>
2018-10-13 23:08:56 -0400

    Add files via upload

---
+++ assets/data/sande.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="sande" tilewidth="64" tileheight="64" tilecount="20" columns="20">
+ <image source="sand.png" width="1280" height="64"/>
+</tileset>
---
+++ assets/data/wawa.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="WaTAHHHH" tilewidth="64" tileheight="64" tilecount="26" columns="2">
+ <image source="WaTAHHHH.png" width="128" height="832"/>
+</tileset>
