755356eb168990a3cf230d0355033f5c40193481
Jordan Powell <powelj87@gmail.com>
2018-10-15 17:15:09 -0400

    removed wrong formatted levels

--- src/objects/Ship.js
+++ src/objects/Ship.js
@@ -12,19 +12,14 @@ export default class Ship extends Phaser.Sprite {
     this.anchor.setTo(.5, .5);
     this.weight = 0;
     this.inputEnabled = true;
-
   }
 
   create() {
-
-    //this.animations.add('right', [5,6,7,8],10, false);
-    //this.animations.add('left', [0,1,2,3], 10, false);
     this.enableBody = true;
     this.physicsBodyType = Phaser.Physics.ARCADE;
     this.game.physics.enable(this, Phaser.Physics.ARCADE);
     this.body.collideWorldBounds = true;
     explode = this.game.add.audio("explosion", 1, false);
-
   }
 
   update() {
@@ -40,7 +35,6 @@ export default class Ship extends Phaser.Sprite {
   movementHandler() {
     this.scale.x = -1;
     this.body.velocity.x = 400;
-
   }
 
 
@@ -49,9 +43,6 @@ export default class Ship extends Phaser.Sprite {
     this.body.velocity.x = 0;
     this.game.isRunning = false;
     this.body.gravity.y = 600;
-
-    // GameStats.running = false;
-
   }
 
   returnToPort() {
@@ -61,10 +52,9 @@ export default class Ship extends Phaser.Sprite {
       this.returnHome();
     }else if(this.x < 2*(this.game.world.centerX) && !returnHome){
       this.continueToEnd();
-    }else if(this.x < 0){
+    }else if(this.x < 300){
       this.game.state.start("Splash");
     }
-
   }
 
   explosion(){
@@ -72,16 +62,15 @@ export default class Ship extends Phaser.Sprite {
   }
 
   returnHome(){
-
     console.log("return home");
     this.scale.x = 1;
     this.body.velocity.x = -1500;
     returnHome = true;
-
   }
+
   continueToEnd(){
     console.log("Continue to end");
     this.body.velocity.x = 800;
-
   }
+
 }
